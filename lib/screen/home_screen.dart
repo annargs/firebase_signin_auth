b0bb02d45e016a5908b70a0d218e1bef6a135c54
annargs <109807632+annargs@users.noreply.github.com>
2022-08-08 17:20:41 +0800

    Add files via upload

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_signin_auth/screen/signin_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Logout"),
+          onPressed: () {
+            FirebaseAuth.instance.signOut().then((value) {
+              print("Signed Out");
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => SignInScreen()));
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
